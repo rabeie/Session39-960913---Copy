f63114c3c266a623028e5226ddef26ca814b75b7
rabeie <rabeie@DESKTOP-G3L4K92>
2017-12-20 17:22:26 +0330

    Add project files.

---
+++ RabieiMoeini/Migrations/201712190956137_mig11.cs
@@ -0,0 +1,16 @@
+namespace RabieiMoeini.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig11 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ RabieiMoeini/SessionDB.cs
@@ -0,0 +1,43 @@
+namespace RabieiMoeini
+{
+    using SessionModel;
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class SessionDB : DbContext
+    {
+
+        public SessionDB()
+            : base("name=SessionDB")
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<Guest> Guests { get; set; }
+        public DbSet<Organ> Organs { get; set; }
+        public DbSet<Indicator> Indicators { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        //   public DbSet<MeetingGuest> MeetingGuests { get; set; }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    //modelBuilder.Entity<Person>()
+        //    //    .Property(p => p.Name)
+        //    //    .HasColumnName("FirstName");
+
+        //    modelBuilder.Entity<Meeting>()
+        //        .HasMany(tp => tp.Guests)
+        //        .WithMany(p => p.Meetings)
+        //        .Map(m => m.ToTable("Meetings_Guests","Meeting"));
+        //}
+
+    }
+    
+}
+   
+
+        
+
+
+   
